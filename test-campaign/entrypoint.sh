7a78771f85f3276ae96f5cf6bd5544404909f7c6
Wivik <48727868+Wivik@users.noreply.github.com>
2024-04-01 14:16:04 +0200

    test

--- test-campaign/entrypoint.sh
+++ test-campaign/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-result=$(python /create-campaign.py -i "${INPUT_ID}" -d "${INPUT_ADDRESS-FROM}")
+result=$(python /test-campaign.py -i "${INPUT_ID}" -d "${INPUT_ADDRESS-FROM}")
 
 echo "result=$result" >> ${GITHUB_OUTPUT}
 
