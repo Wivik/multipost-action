5ac8f5c7a8ad99a6860266b468c6388f94bb0a21
Wivik <48727868+Wivik@users.noreply.github.com>
2024-04-01 16:39:35 +0200

    docker action for test campaign

--- create-campaign/entrypoint.sh
+++ create-campaign/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-result=$(python /create-campaign.py -c "${INPUT_CONTENT}" -s "${INPUT_SUBJECT}" -n "${INPUT_NAME-FROM}" -l "${INPUT_LANG}" -a "${INPUT_ADDRESS-FROM}" -m "${INPUT_MAILINGLISTID}")
+result=$(python /create-campaign.py -c "${INPUT_CONTENT}" -s "${INPUT_SUBJECT}" -n "${INPUT_NAMEFROM}" -l "${INPUT_LANG}" -a "${INPUT_ADDRESSFROM}" -m "${INPUT_MAILINGLISTID}")
 
 echo "result=$result" >> ${GITHUB_OUTPUT}
 
